3af4cd5499a66abbeda89587ef205522a163b469
Xavier MARCELET <xavier@marcelet.com>
2016-12-27 00:38:46 +0100

    debian packaging

--- coverxygen.py
+++ coverxygen.py
@@ -3,7 +3,7 @@
 
 __author__       = "Xavier MARCELET <xavier@marcelet.com>"
 __copyright__    = "Copyright (C) 2016 Xavier MARCELET"
-__version__      = "1.0.0"
+__version__      = "1.0.2"
 __description__  = "Generate doxygen's documentation coverage report"
 __url__          = "https://github.com/psycofdj/coverxygen"
 __download_url__ = "https://github.com/psycofdj/coverxygen/tarball/%s" % __version__
