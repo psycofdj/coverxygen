7d486c95e7e4adde816c6fba4903723f31bff234
Xavier MARCELET <xavier@marcelet.com>
2018-07-02 13:25:49 +0200

    update verison

--- coverxygen/__init__.py
+++ coverxygen/__init__.py
@@ -10,7 +10,7 @@ import xml.etree.ElementTree as ET
 
 __author__       = "Xavier MARCELET <xavier@marcelet.com>"
 __copyright__    = "Copyright (C) 2016 Xavier MARCELET"
-__version__      = "1.3.0"
+__version__      = "1.3.1"
 __description__  = "Generate doxygen's documentation coverage report"
 __url__          = "https://github.com/psycofdj/coverxygen"
 __download_url__ = "https://github.com/psycofdj/coverxygen/tarball/%s" % __version__
